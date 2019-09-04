14ba248edf64715bc6eb80945fed6454a4424bb2
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-09-04 19:17:53 +0100

    wip

--- main.go
+++ main.go
@@ -10,14 +10,19 @@ func main() {
 	// at this stage this pointer is uninitialised.
 	fmt.Println(nameAlpha) // this should output 'nil', this is go's way of saying the pointer isn't point to anything
 
-	// singer it is unitialised it means we can't story any data at the pointer's memory location. hence the following will fail:
+	// since it is unitialised it means we can't story any data at the pointer's memory location. hence the following will fail:
 	// *nameAlpha = Peter
-	// Note we use the asterisk notation to indicate that we're working with a pointer datatype
+	// Note we use the asterisk notation to indicate that we're working with a pointer datatype's memory location
 
 	// This time we initialise it, which in earmarks some memory
 	var nameBravo *string = new(string)
 	// this outputs the memory location, e.g. something like 0x53x10
 	fmt.Println(nameBravo)
+
+	// to get the value stored at this memory location we do
+	// the following, at this point it will print a blank line
+	// since that location is empty.
+	fmt.Println(*nameBravo)
 
 	// now we can store a variable value at our pointer's memory locaitoni
 	*nameBravo = "Simon"
